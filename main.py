71496837180a343f8c1ce4691f341b67f98f5f69
Kseniia <kseniiailchenko713@gmail.com>
2022-08-25 18:51:30 -0400

    Ksusha

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import turtle
 
 mainTurtle = turtle.Turtle()
-
-
+def ksusha(x,y):
+    mainTurtle.penup()
+    mainTurtle.pencolor('red')
+    mainTurtle.goto(x,y)
+    mainTurtle.pendown()
+    mainTurtle.forward(50)
+    mainTurtle.left(72)
+    mainTurtle.forward(50)
+    mainTurtle.left(72)
+    mainTurtle.forward(50)
+    mainTurtle.left(72)
+    mainTurtle.forward(50)
+    mainTurtle.left(72)
+    mainTurtle.forward(50)
+ksusha(x=-50, y=-0)
 
 mainTurtle.hideturtle()
 turtle.exitonclick()
